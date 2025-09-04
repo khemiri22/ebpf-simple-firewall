7b6d24a8acc41070e8a0163708d1863294886511
khemiri22 <khe.amine22@gmail.com>
2025-09-04 03:19:34 +0100

    fixing endianess

--- simple_firewall.py
+++ simple_firewall.py
@@ -26,11 +26,11 @@ def detach_xdp_program(bpf, interface):
 # Callback function to print debug events
 def print_debug_event(cpu, data, size):
     src_ip = ctypes.cast(data, ctypes.POINTER(ctypes.c_uint32)).contents.value
-    print(f"Packet from {socket.inet_ntoa(struct.pack('!I', src_ip))} dropped")
+    print(f"Packet from {socket.inet_ntoa(struct.pack('I', src_ip))} dropped")
 
 # Function to add an IP address to the block list
 def block_ip(bpf, ip_addr_str):
-    ip_addr = struct.unpack("!I", socket.inet_aton(ip_addr_str))[0]
+    ip_addr = struct.unpack("I", socket.inet_aton(ip_addr_str))[0] 
     bpf["blocked_ips_map"][ctypes.c_uint32(ip_addr)] = ctypes.c_uint8(1)
     print(f"Blocked IP added: {ip_addr_str}")
 
